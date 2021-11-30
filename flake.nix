2bd1297596a7125b3b8a671d2c4a9b10cc84c5fd
Michael Hueschen <m@mhueschen.space>
2021-11-29 18:56:34 -0700

    wat

--- flake.nix
+++ flake.nix
@@ -101,7 +101,7 @@
               text = ''
                 import init, { run_app } from './app.js';
                 async function main() {
-                   await init('nix_build_rust_wasm_example.wasm');
+                   await init('./nix_build_rust_wasm_example.wasm');
                    run_app();
                 }
                 main()
